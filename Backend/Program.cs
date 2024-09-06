f48e1cdc0de1740b9631d9fb0223f565f9d91554
fatimaaa10 <fatima.khan@sportzinteractive.net>
2024-09-06 15:45:31 +0530

    API endpoints

---
+++ Backend/Program.cs
@@ -0,0 +1,49 @@
+using ChessApiDemo.DAO;
+using ChessApiDemo.Controllers;
+using Npgsql;
+namespace ChessApiDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("PostgreDB");
+            builder.Services.AddScoped((provider) => new NpgsqlConnection(connectionString));
+
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAny", builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+                options.AddPolicy("FrontEndClient", builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5173"));
+            });
+
+            builder.Services.AddScoped<IChessDAO, ChessDAOImpl>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.UseCors("FrontEndClient");
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
